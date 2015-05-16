4e03a6aa0b73c4ca6bd43ed69758c0aa9e576d64
Eric Lin <lin.guyuan@gmail.com>
2015-05-16 21:09:51 +0800

    完成 GeoDjango 在 local 端使用 SpatialLite 設定

--- trytalk/settings/local.py
+++ trytalk/settings/local.py
@@ -1,1 +1,8 @@
 from default import *
+
+DEBUG = True
+
+TEMPLATE_DEBUG = DEBUG
+
+# For GeoDjango to be able to find the SpatiaLite library
+SPATIALITE_LIBRARY_PATH='/usr/local/lib/mod_spatialite.dylib'
