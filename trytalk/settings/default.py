969df10e1b7cb009f008cbeff4c1c8f5788f3497
Eric Lin <lin.guyuan@gmail.com>
2015-05-16 21:28:07 +0800

    修改 local Database 的 backend

--- trytalk/settings/default.py
+++ trytalk/settings/default.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
 )
 
 MIDDLEWARE_CLASSES = (
--- trytalk/settings/local.py
+++ trytalk/settings/local.py
@@ -4,5 +4,14 @@ DEBUG = True
 
 TEMPLATE_DEBUG = DEBUG
 
+# Using SpatialLite backend
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.spatialite',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
 # For GeoDjango to be able to find the SpatiaLite library
 SPATIALITE_LIBRARY_PATH='/usr/local/lib/mod_spatialite.dylib'
